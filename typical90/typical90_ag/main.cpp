58e1fbee2966aea98281500c3743ecf5d95da885
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2021-06-09 20:54:05 +0900

    典型90問-33日目、自力AC

---
+++ typical90/typical90_ag/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+//#include <atcoder/all>
+using namespace std;
+//using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+// 自力AC
+
+int main() {
+	int h, w;
+	cin >> h >> w;
+	if (h<=1 || w<=1) cout << max(h, w) << endl;
+	else cout << (((h+1)/2) * ((w+1)/2)) << endl;
+	return 0;
+}
