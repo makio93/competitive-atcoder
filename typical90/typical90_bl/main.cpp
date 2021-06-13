9d60cce9f2b6225d2ecdde0ef29de24c7d1fce93
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2021-06-14 08:49:55 +0900

    典型90問-65日目を自力で解答、フォルダーを作り直した

---
+++ typical90/typical90_bl/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+// 自力AC
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+    vector<ll> a(n+1), b(n+1);
+    rep(i, n) cin >> a[i];
+    rep(i, n) b[i+1] = a[i+1] - a[i];
+    ll val = 0;
+    rep(i, n-1) val += llabs(b[i+1]);
+    rep(i, q) {
+        int li, ri; ll vi;
+        cin >> li >> ri >> vi;
+        --li;
+        ll lsub = (li > 0) ? (llabs(b[li]+vi)-llabs(b[li])) : 0;
+        ll rsub = (ri < n) ? (llabs(b[ri]-vi)-llabs(b[ri])) : 0;
+        b[li] += vi; b[ri] -= vi;
+        val += lsub + rsub;
+        cout << val << endl;
+    }
+    return 0;
+}
