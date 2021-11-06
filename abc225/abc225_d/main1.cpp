ca4aaa97422d2f8cf11092ffe80def1d731c386b
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2021-11-07 05:55:10 +0900

    [AC] ABC225-D,E問題,解説AC

---
+++ abc225/abc225_d/main1.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+// 解説AC
+
+int main() {
+	int n, q;
+	cin >> n >> q;
+	vector<int> fid(n, -1), bid(n, -1);
+	rep(i, q) {
+		int qi;
+		cin >> qi;
+		if (qi == 1) {
+			int xi, yi;
+			cin >> xi >> yi;
+			--xi; --yi;
+			fid[yi] = xi;
+			bid[xi] = yi;
+		}
+		else if (qi == 2) {
+			int xi, yi;
+			cin >> xi >> yi;
+			--xi; --yi;
+			fid[yi] = -1;
+			bid[xi] = -1;
+		}
+		else {
+			int xi;
+			cin >> xi;
+			--xi;
+			int pid = xi;
+			while (fid[pid] != -1) pid = fid[pid];
+			vector<int> res;
+			while (pid != -1) {
+				res.push_back(pid+1);
+				pid = bid[pid];
+			}
+			int m = res.size();
+			cout << m << ' ';
+			rep(j, m) printf("%d%c", res[j], (j<m-1?' ':'\n'));
+		}
+	}
+	return 0;
+}
---
+++ abc225/abc225_e/main1.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+// 解説AC
+
+class fraction {
+    ll num, den;
+public:
+    fraction(ll num=0, ll den=1) : num(num), den(den) {}
+    bool operator<(const fraction& other) const {
+        return num * other.den < other.num * den;
+    }
+    bool operator<=(const fraction& other) const {
+        return num * other.den <= other.num * den;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> x(n), y(n);
+    rep(i, n) cin >> x[i] >> y[i];
+    vector<pair<fraction, fraction>> flst(n);
+    rep(i, n) flst[i] = { fraction(y[i], x[i]-1), fraction(y[i]-1, x[i]) };
+    sort(all(flst));
+    int res = 0, nid = 0;
+    while (nid < n) {
+        ++res;
+        int tid = nid + 1;
+        while (tid<n && flst[tid].second<flst[nid].first) ++tid;
+        nid = tid;
+    }
+    cout << res << endl;
+    return 0;
+}
