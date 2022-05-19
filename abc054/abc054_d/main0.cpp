18cde05c26f25adaeb16c33e0d62d5ad30fa3915
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-05-19 14:17:13 +0900

    [AC] ABC054-D,学習0回目,自力AC+解説AC

---
+++ abc054/abc054_d/main0.cpp
@@ -0,0 +1,49 @@
+// 学習0回目,自力AC
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+const int INF = (int)(1e9);
+
+int main() {
+	int n, ma, mb;
+	cin >> n >> ma >> mb;
+	vector<int> a(n), b(n), c(n);
+	rep(i, n) cin >> a[i] >> b[i] >> c[i];
+	map<pair<int, int>, int> lval, rval;
+	rep(i, (1<<(n/2))) {
+		int ai = 0, bi = 0, ci = 0;
+		rep(j, n/2) if ((i>>j)&1) { ai += a[j], bi += b[j], ci += c[j]; }
+		if (lval.find({ai, bi}) == lval.end()) lval[{ai, bi}] = ci;
+		else lval[{ai, bi}] = min(lval[{ai, bi}], ci);
+	}
+	rep(i, (1<<(n-n/2))) {
+		int ai = 0, bi = 0, ci = 0;
+		rep(j, n-n/2) if ((i>>j)&1) { ai += a[n/2+j], bi += b[n/2+j], ci += c[n/2+j]; }
+		if (rval.find({ai, bi}) == rval.end()) rval[{ai, bi}] = ci;
+		else rval[{ai, bi}] = min(rval[{ai, bi}], ci);
+	}
+	int tar = 1;
+	int res = INF;
+	while (ma*tar<=400 && mb*tar<=400) {
+		int ta = ma*tar, tb = mb*tar;
+		for (auto pi : lval) if (pi.first.first<=ta && pi.first.second<=tb) {
+			int ra = ta - pi.first.first, rb = tb - pi.first.second;
+			if (rval.find({ra, rb}) != rval.end()) res = min(res, pi.second+rval[{ra, rb}]);
+		}
+		++tar;
+	}
+	if (res == INF) cout << -1 << endl;
+	else cout << res << endl;
+	return 0;
+}
---
+++ abc054/abc054_d/main1.cpp
@@ -0,0 +1,37 @@
+// 学習0回目,解説AC
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+const int INF = (int)(1e9);
+
+int main() {
+	int n, ma, mb;
+	cin >> n >> ma >> mb;
+	vector<int> a(n), b(n), c(n);
+	rep(i, n) cin >> a[i] >> b[i] >> c[i];
+	int asum = 0, bsum = 0;
+	rep(i, n) asum += a[i];
+	rep(i, n) bsum += b[i];
+	vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(asum+1, vector<int>(bsum+1, INF)));
+	dp[0][0][0] = 0;
+	rep(i, n) rep(j1, asum+1) rep(j2, bsum+1) if (dp[i][j1][j2] != INF) {
+		dp[i+1][j1][j2] = min(dp[i+1][j1][j2], dp[i][j1][j2]);
+		dp[i+1][j1+a[i]][j2+b[i]] = min(dp[i+1][j1+a[i]][j2+b[i]], dp[i][j1][j2]+c[i]);
+	}
+	int res = INF;
+	rep3(i, 1, asum+1) rep3(j, 1, bsum+1) if (i*mb == j*ma) res = min(res, dp[n][i][j]);
+	if (res == INF) res = -1;
+	cout << res << endl;
+	return 0;
+}
