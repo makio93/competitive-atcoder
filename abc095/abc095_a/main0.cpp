fe99b3a4cb99180b17018354b59524bf5a389fe7
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-05-20 21:35:19 +0900

    [AC] ABC095-A,自力AC

---
+++ abc095/abc095_a/main0.cpp
@@ -0,0 +1,25 @@
+// 自力AC
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+const int INF = (int)(1e9);
+
+int main() {
+	string s;
+	cin >> s;
+	int res = 700;
+	rep(i, 3) if (s[i] == 'o') res += 100;
+	cout << res << endl;
+	return 0;
+}
