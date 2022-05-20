bda50cae442b538ce0e7f561f5cea58c0aec39b3
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-05-20 11:53:04 +0900

    [AC] ABC154-C,学習1回目-自力AC

---
+++ abc154/abc154_c/1/main0.cpp
@@ -0,0 +1,28 @@
+// 学習1回目,自力AC
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+const int INF = (int)(1e9);
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	rep(i, n) cin >> a[i];
+	sort(all(a));
+	a.erase(unique(all(a)), a.end());
+	if (n == (int)(a.size())) cout << "YES" << endl;
+	else cout << "NO" << endl;
+	return 0;
+}
