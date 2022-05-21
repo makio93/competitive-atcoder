d309c1619efcbad9a20987f6717c335513b91901
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-05-21 11:37:52 +0900

    [AC] ABC178A～E:本番参加分

---
+++ abc178/abc178_d/main0.cpp
@@ -0,0 +1,85 @@
+// 本番AC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pii  = pair<int, int>;
+using pll = pair<ll, ll>;
+#define ull unsigned long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; i++)
+#define rep1(i, n) for (int i = 1, i##_len = (n); i <= i##_len; i++)
+#define repr(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define rep1r(i, n) for (int i = ((int)(n)); i >= 1; i--)
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define RSORT(x) sort(rall(x));
+#define pb push_back
+#define mp make_pair
+
+#define INF (1e9)
+#define PI (acos(-1))
+#define EPS (1e-7)
+
+ull gcd(ull a, ull b) { return b ? gcd(b, a % b) : a; }
+ull lcm(ull a, ull b) { return a / gcd(a, b) * b; }
+
+const long long mod = 1000000007;
+struct mint {
+    ll x;
+    mint(ll x=0):x((x%mod+mod)%mod){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+    mint inv() const { return pow(mod-2);}
+    mint& operator/=(const mint a) { return *this *= a.inv();}
+    mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x; }
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x; }
+
+int main(){
+    int s;
+    cin >> s;
+    vector<mint> dp(s+1);
+    rep1(i, s) {
+        if (i<=2) continue;
+        if (i-3<0) continue;
+        dp[i] = 1;
+        for (int j=3; i-j>=0; ++j) {
+            dp[i] += dp[i-j];
+        }
+    }
+    cout << dp[s] << endl;
+    return 0;
+}
