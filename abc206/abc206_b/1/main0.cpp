79de6a1d9abf615b36ba12546213853704608282
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-05-25 10:33:28 +0900

    [AC] ABC206-B:バチャ参加:自力AC-追加

---
+++ abc206/abc206_b/1/main0.cpp
@@ -0,0 +1,28 @@
+// バチャ,自力AC
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+const int INF = (int)(1e9);
+
+int main() {
+	int n;
+	cin >> n;
+	int res = 0, val = 0;
+	while (val < n) {
+		++res;
+		val += res;
+	}
+	cout << res << endl;
+	return 0;
+}
