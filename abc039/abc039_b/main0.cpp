1f5214b4fd6571609e90b971736b3ecb51eb5c83
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-06-04 02:31:08 +0900

    [AC] ABC039-B:バチャ参加-自力AC+解説AC

---
+++ abc039/abc039_b/main0.cpp
@@ -0,0 +1,25 @@
+// バチャ,自力AC
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+const int INF = (int)(1e9);
+
+int main() {
+	int x;
+	cin >> x;
+	int res = 0;
+	while (res*res*res*res < x) ++res;
+	cout << res << endl;
+	return 0;
+}
