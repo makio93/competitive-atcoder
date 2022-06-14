d55bdc8c5df9ccb21fd1c04b1034e31bd0d7d21b
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-06-14 13:18:13 +0900

    [AC] ABC158-A:学習1回目(バチャ参加):自力AC

---
+++ abc158/abc158_a/1/main0.cpp
@@ -0,0 +1,24 @@
+// 学習1回目,バチャ参加,自力AC
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+const int INF = (int)(1e9);
+
+int main() {
+	string s;
+	cin >> s;
+	if (s[0]!=s[1] || s[1]!=s[2]) cout << "Yes" << endl;
+	else cout << "No" << endl;
+	return 0;
+}
