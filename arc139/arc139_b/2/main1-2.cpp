6313e545c6152775f59a5b39338346e327848d68
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-07-08 23:21:11 +0900

    [AC] ARC139-B:復習2回目:解説AC1-2追加+ファイルの移動

---
+++ arc139/arc139_b/2/main1-2.cpp
@@ -0,0 +1,44 @@
+// 復習2回目,解説AC2
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+ll gcd(ll a, ll b) { return (b ? gcd(b, a%b) : a); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b) * b); }
+
+const ll LINF = (ll)(8e18);
+
+int main() {
+    int t0;
+    cin >> t0;
+    rep(i0, t0) {
+        ll n, a, b, x, y, z;
+        cin >> n >> a >> b >> x >> y >> z;
+        y = min(y, x*a);
+        z = min(z, x*b);
+        if (y*b > z*a) {
+            swap(a, b);
+            swap(y, z);
+        }
+        ll res = LINF, lval = lcm(a, b);
+        if (n/a < lval/b) for (ll i=0; i<=n/a; ++i) res = min(res, i*y+(n-i*a)/b*z+(n-i*a)%b*x);
+        else {
+            for (ll i=0; i<lval/b; ++i) {
+                if (i*b > n) break;
+                res = min(res, i*z+(n-i*b)/a*y+(n-i*b)%a*x);
+            }
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
