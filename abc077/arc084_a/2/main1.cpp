b4c54e8b3dd4afe4bfc652be572918cbdf85d10f
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-07-30 11:14:11 +0900

    [AC] ABC077-C:復習2回目:解説AC

---
+++ abc077/arc084_a/2/main1.cpp
@@ -0,0 +1,30 @@
+// 解説AC
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n), c(n);
+    rep(i, n) cin >> a[i];
+    rep(i, n) cin >> b[i];
+    rep(i, n) cin >> c[i];
+    sort(all(a));
+    sort(all(b));
+    sort(all(c));
+    ll res = 0;
+    rep(i, n) res += distance(a.begin(), lower_bound(all(a), b[i])) * distance(upper_bound(all(c), b[i]), c.end());
+    cout << res << endl;
+    return 0;
+}
