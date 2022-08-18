18c255219407a589955f2d285dff0eb237d80662
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-08-19 08:57:48 +0900

    [AC] ABC032-B:自力AC

---
+++ abc032/abc032_b/main0.cpp
@@ -0,0 +1,27 @@
+// 自力AC
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+const int INF = (int)(1e9);
+
+int main() {
+	string s;
+	cin >> s;
+	int n = s.length(), k;
+	cin >> k;
+	unordered_set<string> st;
+	for (int i=0; i+k-1<n; ++i) st.insert(s.substr(i, k));
+	cout << (int)(st.size()) << endl;
+	return 0;
+}
