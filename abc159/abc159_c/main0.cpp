5e739d1e7d37398861eb604b56af81653f620090
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-08-21 02:03:08 +0900

    [AC] ABC159:本番参加分:自力AC+解説AC

---
+++ abc159/abc159_c/main0.cpp
@@ -0,0 +1,59 @@
+// 本番AC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// 総数を1000000007（素数）で割った余り
+const long long mod = 1e9 + 7;
+
+using ll = long long;
+using pii  = pair<int, int>;
+using pll = pair<ll, ll>;
+#define ull unsigned long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; i++)
+#define rep1(i, n) for (int i = 1, i##_len = (n); i <= i##_len; i++)
+#define repr(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define rep1r(i, n) for (int i = ((int)(n)); i >= 1; i--)
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define RSORT(x) sort(rall(x));
+#define pb push_back
+#define mp make_pair
+
+#define INF (1e9)
+#define PI (acos(-1))
+#define EPS (1e-7)
+
+ull gcd(ull a, ull b) { return b ? gcd(b, a % b) : a; }
+ull lcm(ull a, ull b) { return a / gcd(a, b) * b; }
+
+int main(){
+    int l;
+    cin >> l;
+    long double ans = l / 3.0;
+    ans = ans * ans * ans;
+    printf("%llf\n", ans);
+    //cout << ans << endl;
+    /*
+    ull ans = l * 100 / 3;
+    ans = ans * ans * ans;
+    ull a, b;
+    a = ans / 1000000;
+    b = ans % 1000000;
+    cout << a << '.' << b << endl;
+    */
+    return 0;
+}
