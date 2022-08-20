bdc78818ba4377b6247472cb8cfa4b73be920d5d
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-08-21 03:45:38 +0900

    [AC] 本番参加分:コメント部分の軽微な修正

--- abc237/abc237_f/main0.cpp
+++ abc237/abc237_f/main0.cpp
@@ -1,3 +1,5 @@
+// 本番,解答不能
+
 #include <bits/stdc++.h>
 #include <atcoder/all>
 using namespace std;
@@ -10,8 +12,6 @@ using ll = long long;
 #define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
 #define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
 #define all(x) (x).begin(), (x).end()
-
-// 本番,解答不能
 
 const ll mod = 998244353LL;
 struct mint {
