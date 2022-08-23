b6cab69ee288369f95bc42fe26aaf8d84ee9b90b
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-08-24 00:52:14 +0900

    [AC] ABC043-B:学習0回目+1回目:自力AC

---
+++ abc043/abc043_b/1/main0.cpp
@@ -0,0 +1,28 @@
+// 自力AC
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+const int INF = (int)(1e9);
+
+int main() {
+	string s;
+	cin >> s;
+	string res;
+	for (char& ci : s) {
+		if (ci != 'B') res.push_back(ci);
+		else if (!res.empty()) res.pop_back();
+	}
+	cout << res << endl;
+	return 0;
+}
