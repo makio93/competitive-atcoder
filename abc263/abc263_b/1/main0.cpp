4517ba481d48b95b01b13ff984b200c759e7fd3a
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-08-24 23:34:43 +0900

    [AC] ABC263-B:学習1回目:自力AC

---
+++ abc263/abc263_b/1/main0.cpp
@@ -0,0 +1,33 @@
+// 学習1回目,自力AC
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+const int INF = (int)(1e9);
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> p(n);
+	rep3(i, 1, n) {
+		cin >> p[i];
+		p[i]--;
+	}
+	int res = 0, pid = n-1;
+	while (pid != 0) {
+		pid = p[pid];
+		++res;
+	}
+	cout << res << endl;
+	return 0;
+}
