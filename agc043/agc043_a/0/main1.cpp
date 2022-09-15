98b12300193e61f145cab927140c30fea541073a
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-09-15 20:36:13 +0900

    [AC] 過去ソースのいくつかを追加

---
+++ agc043/agc043_a/0/main1.cpp
@@ -0,0 +1,75 @@
+// 解説AC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// 総数を1000000007（素数）で割った余り
+const long long mod = 1e9 + 7;
+
+using ll = long long;
+using pii  = pair<int, int>;
+using pll = pair<ll, ll>;
+#define ull unsigned long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; i++)
+#define rep1(i, n) for (int i = 1, i##_len = (n); i <= i##_len; i++)
+#define repr(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define rep1r(i, n) for (int i = ((int)(n)); i >= 1; i--)
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define RSORT(x) sort(rall(x));
+#define pb push_back
+#define mp make_pair
+
+#define INF (1e9)
+#define PI (acos(-1))
+#define EPS (1e-7)
+
+ull gcd(ull a, ull b) { return b ? gcd(b, a % b) : a; }
+ull lcm(ull a, ull b) { return a / gcd(a, b) * b; }
+
+bool mt[101][101];
+int cnt[101][101];
+
+int main(){
+    int h, w;
+    cin >> h >> w;
+    rep1(i, h) {
+        rep1(j, w) {
+            char m;
+            cin >> m;
+            if (m=='.') mt[i][j] = true;
+        }
+    }
+    for (int i=2; i<=w+h; i++) {
+        for (int ch=1; ch<i; ch++) {
+            int cw = i - ch;
+            if (cw > w || ch > h) continue;
+            if (ch==1&&cw==1) 
+                cnt[ch][cw] = mt[ch][cw]?0:1;
+            else if (ch==1) 
+                cnt[ch][cw] = (mt[ch][cw-1]==true&&mt[ch][cw]==false)?(cnt[ch][cw-1]+1):cnt[ch][cw-1];
+            else if (cw==1) 
+                cnt[ch][cw] = (mt[ch-1][cw]==true&&mt[ch][cw]==false)?(cnt[ch-1][cw]+1):cnt[ch-1][cw];
+            else {
+                int right = (mt[ch][cw-1]==true&&mt[ch][cw]==false)?(cnt[ch][cw-1]+1):cnt[ch][cw-1];
+                int height = (mt[ch-1][cw]==true&&mt[ch][cw]==false)?(cnt[ch-1][cw]+1):cnt[ch-1][cw];
+                cnt[ch][cw] = min(right, height);
+            }
+        }
+    }
+    cout << cnt[h][w] << endl;
+    return 0;
+}
---
+++ jsc2019-qual/jsc2019_qual_b/main1.cpp
@@ -0,0 +1,90 @@
+// 解説AC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pii  = pair<int, int>;
+using pll = pair<ll, ll>;
+#define ull unsigned long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; i++)
+#define rep1(i, n) for (int i = 1, i##_len = (n); i <= i##_len; i++)
+#define repr(i, n) for (int i = ((int)(n)-1); i >= 0; i--)
+#define rep1r(i, n) for (int i = ((int)(n)); i >= 1; i--)
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define RSORT(x) sort(rall(x));
+#define pb push_back
+#define mp make_pair
+
+#define INF (1e9)
+#define PI (acos(-1))
+#define EPS (1e-7)
+
+ull gcd(ull a, ull b) { return b ? gcd(b, a % b) : a; }
+ull lcm(ull a, ull b) { return a / gcd(a, b) * b; }
+
+const long long mod = 1000000007;
+// auto mod int
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x=0):x((x%mod+mod)%mod){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+    mint operator+(const mint a) const { return mint(*this) += a;}
+    mint operator-(const mint a) const { return mint(*this) -= a;}
+    mint operator*(const mint a) const { return mint(*this) *= a;}
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const { return pow(mod-2);}
+    mint& operator/=(const mint a) { return *this *= a.inv();}
+    mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x; }
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x; }
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    vi a(n);
+    rep(i, n) cin >> a[i];
+    int p = 0, q = 0;
+    rep(i, n) for (int j=i+1; j<n; ++j) {
+        if (a[i] > a[j]) ++p;
+    }
+    rep(i, n) rep(j, n) {
+        if (i == j) continue;
+        if (a[j] > a[i]) ++q;
+    }
+    mint ans = (mint)p * k + (mint)q * k * (k-1) / 2;
+    cout << ans << endl;
+    return 0;
+}
