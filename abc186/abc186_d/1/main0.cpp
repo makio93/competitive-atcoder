5a478530339ce46aed05008f58a9d6fa358f23b2
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-10-19 01:43:12 +0900

    [AC] ABC186-D:学習1回目:バチャ本番AC

---
+++ abc186/abc186_d/1/main0.cpp
@@ -0,0 +1,31 @@
+// 学習1回目,バチャ本番AC
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+const int INF = (int)(1e9);
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	rep(i, n) cin >> a[i];
+	sort(all(a));
+	ll res = 0, sum = 0;
+	rep(i, n) {
+		res += (ll)a[i] * i - sum;
+		sum += a[i];
+	}
+	cout << res << endl;
+	return 0;
+}
