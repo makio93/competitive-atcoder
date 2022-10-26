bd6a6b2171ca0389b69320b4534e961f901b4e01
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-10-26 14:34:02 +0900

    [AC] ABC206-A:解説AC

---
+++ abc206/abc206_a/main1-2.cpp
@@ -0,0 +1,24 @@
+// 解説AC2
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+int main() {
+    int n;
+    cin >> n;
+    double r = n * 1.08;
+    if (r < 206.0) cout << "Yay!" << endl;
+    else if (r < 207.0) cout << "so-so" << endl;
+    else cout << ":(" << endl;
+    return 0;
+}
---
+++ abc206/abc206_a/main1-3.cpp
@@ -0,0 +1,23 @@
+// 
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+int main() {
+    int n;
+    cin >> n;
+    if (n < 191) cout << "Yay!" << endl;
+    else if (n == 191) cout << "so-so" << endl;
+    else cout << ":(" << endl;
+    return 0;
+}
