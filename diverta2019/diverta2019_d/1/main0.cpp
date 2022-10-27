82cc3f67f0527db68e0d89c77dea92811f774c04
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2022-10-28 05:15:31 +0900

    [AC] DIVERTA2019-D:学習1回目:自力AC

---
+++ diverta2019/diverta2019_d/1/main0.cpp
@@ -0,0 +1,25 @@
+// 学習1回目,自力AC
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+int main() {
+    ll n;
+    cin >> n;
+    ll res = 0;
+    for (ll i=1; i*i<=n; ++i) if (n%i == 0) {
+        if (n/i >= i+2) res += n/i - 1;
+    }
+    cout << res << endl;
+    return 0;
+}
