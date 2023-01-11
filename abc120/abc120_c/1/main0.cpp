19d6bee2bcf22f67032cfa465ad04a76f9963a4e
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2023-01-12 01:04:59 +0900

    [AC] ABC120-C:学習1回目:自力AC

---
+++ abc120/abc120_c/1/main0.cpp
@@ -0,0 +1,22 @@
+// 学習1回目,自力AC
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+int main() {
+    string s;
+    cin >> s;
+    int n = s.length(), cnt = count(all(s), '0');
+    cout << (min(cnt, n-cnt) * 2) << endl;
+    return 0;
+}
