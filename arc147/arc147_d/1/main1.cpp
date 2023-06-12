e9962c9466ef9584d378576eb89be79ca04fd70b
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2023-06-12 20:18:23 +0900

    [AC] ARC147-D:学習1回目:解説AC

---
+++ arc147/arc147_d/1/main1.cpp
@@ -0,0 +1,24 @@
+// 学習1回目,解説AC
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+using mint = modint998244353;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    mint res = mint(n).pow(m) * mint(m).pow(n-1);
+    cout << res.val() << endl;
+    return 0;
+}
