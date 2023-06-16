1b9edd16c083ed201ef45db5b5ccb0737ce9b706
norikame <mergon-nitro-pale_fluorelight@hotmail.co.jp>
2023-06-16 18:58:03 +0900

    [AC] ABC185-E:学習3回目:自力AC

---
+++ abc185/abc185_e/3/main0.cpp
@@ -0,0 +1,34 @@
+// 学習3回目,自力AC
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+using ll = long long;
+
+#define rep(i, n) for (int i=0; i<(int)(n); ++(i))
+#define rep3(i, m, n) for (int i=(m); (i)<(int)(n); ++(i))
+#define repr(i, n) for (int i=(int)(n)-1; (i)>=0; --(i))
+#define rep3r(i, m, n) for (int i=(int)(n)-1; (i)>=(int)(m); --(i))
+#define all(x) (x).begin(), (x).end()
+
+const int INF = (int)(1e9);
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    rep(i, n) cin >> a[i];
+    rep(i, m) cin >> b[i];
+    vector<vector<int>> dp(n+1, vector<int>(m+1, INF));
+    rep(i, n+1) dp[i][0] = i;
+    rep(i, m+1) dp[0][i] = i;
+    rep(i, n) rep(j, m) {
+        if (a[i] == b[j]) dp[i+1][j+1] = dp[i][j];
+        else dp[i+1][j+1] = dp[i][j] + 1;
+        dp[i+1][j+1] = min({ dp[i+1][j+1], dp[i][j+1]+1, dp[i+1][j]+1 });
+    }
+    cout << dp[n][m] << endl;
+    return 0;
+}
